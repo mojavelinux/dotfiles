09898c399f9f20779da13523ea5f128fdbbda2dd
Dan Allen <dan.j.allen@gmail.com>
2012-01-23 15:13:24 -0500

    jruby home

--- bash/homes.sh
+++ bash/homes.sh
@@ -20,7 +20,8 @@ export PYTHONPATH=$PYTHON_EXT_HOME/lib/python2.6/site-packages
 export FORGE_HOME=$HOME/opt/forge
 #export GEM_HOME=/var/lib/gems/1.8
 export TORQUEBOX_HOME=$HOME/opt/torquebox
-export JRUBY_HOME=$TORQUEBOX_HOME/jruby
+#export JRUBY_HOME=$TORQUEBOX_HOME/jruby
+export JRUBY_HOME=$HOME/opt/jruby
 # we get a lot of fake mozilla/firefox directories, so detect a real one by looking for the chrome directory
 #export MOZILLA_FIVE_HOME=`ls -1rd /usr/lib/*firefox*/chrome 2>/dev/null | head -1 | xargs dirname 2>/dev/null`
 #if [ -d $HOME/.mozilla/plugins ] ; then
