0ffd4aa643e046f3399603687d16ae72e0c77dc4
Dan Allen <dan.j.allen@gmail.com>
2021-02-28 02:37:19 -0700

    update skel files for vim

--- vim/skel.java
+++ vim/skel.java
@@ -3,8 +3,8 @@ import static java.lang.System.*;
 
 public class Test {
 
-	public static void main( String[] args ) {
-		out.println();
-	}
+    public static void main(String[] args) {
+        out.println();
+    }
 
 }
